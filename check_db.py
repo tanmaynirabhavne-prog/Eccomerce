5fb1f6e10e1437cb4988e4aad503617e64812e01
Tanmay Nirabhavne <tanmaynirabhavne@gmail.com>
2026-07-29 12:45:58 +0530

    remove postgres env, db, pycache, and ml artifacts from git; add gitignore

--- check_db.py
+++ check_db.py
@@ -1,4 +1,7 @@
-﻿import sqlite3
+﻿
+
+
+import sqlite3
 conn = sqlite3.connect("ecommerce.db")
 tables = conn.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
 print("Tables:", tables)
